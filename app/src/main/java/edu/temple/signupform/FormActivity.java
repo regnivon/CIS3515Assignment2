74ef1678286d1fc32e3d5621823ca753dd70ddf0
regnivon <qdnovinger@gmail.com>
2021-02-10 16:17:01 -0500

    retriving views in code and handling a submit

---
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -0,0 +1,96 @@
+package edu.temple.signupform;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FormActivity extends AppCompatActivity {
+
+    private final int PASSWORD = 2;
+    private final int PASSWORD_CONFIRM = 3;
+    private final int PASSWORD_MISMATCH = 4;
+
+    Button submit;
+
+    List<EditText> inputFields;
+    List<TextView> errorFields;
+    List<String> errorTexts;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       submit = findViewById(R.id.submitButton);
+
+
+        inputFields = new ArrayList<>(
+                Arrays.asList(
+                        (EditText)findViewById(R.id.emailInput),
+                        (EditText)findViewById(R.id.usernameInput),
+                        (EditText)findViewById(R.id.passwordInput),
+                        (EditText)findViewById(R.id.confirmPasswordInput)
+                )
+        );
+
+        errorFields = new ArrayList<>(
+                Arrays.asList(
+                        (TextView)findViewById(R.id.emailErrorView),
+                        (TextView)findViewById(R.id.usernameErrorView),
+                        (TextView)findViewById(R.id.passwordErrorView),
+                        (TextView)findViewById(R.id.confirmPasswordErrorView)
+                )
+        );
+
+        errorTexts = new ArrayList<>(
+                Arrays.asList(
+                    getString(R.string.emailError),
+                    getString(R.string.usernameError),
+                    getString(R.string.passwordError),
+                    getString(R.string.confirmPasswordError),
+                    getString(R.string.confirmPasswordMismatchError)
+                )
+        );
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitPress();
+            }
+        });
+    }
+
+    private void submitPress() {
+        boolean error = false;
+        for (TextView e: errorFields) {
+            e.clearComposingText();
+        }
+        for (int i = 0; i < inputFields.size(); i++) {
+            if (TextUtils.isEmpty(inputFields.get(i).getText())) {
+                errorFields.get(i).setText(errorTexts.get(i));
+                error = true;
+            }
+        }
+
+        if (TextUtils.equals(inputFields.get(PASSWORD).getText().toString(),
+                inputFields.get(PASSWORD_CONFIRM).getText().toString()))
+        {
+            errorFields.get(PASSWORD).setText(errorTexts.get(PASSWORD_MISMATCH));
+            error = true;
+        }
+
+        if (!error) {
+
+        }
+    }
+}
